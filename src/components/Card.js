b6be91f629596936e1e4b112c13991e377f605df
ram5353 <ramkiran5353@gmail.com>
2022-06-16 22:17:34 -0400

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import './Card.css'
+
+function Card(props) {
+
+    const clases = 'card ' + props.className
+    return (
+        <div className={clases}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Card
